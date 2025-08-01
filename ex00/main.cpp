a22b41a0ca4e0830215b00b7043cdebdaa839fe8
root <root@LAPTOP-IL61TBPL.localdomain>
2025-08-01 13:17:39 +0200

    first exercise

---
+++ ex00/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cctype>
+
+int	main (int argc, char *argv[])
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+	else
+	{
+		while (argv[i + 1])
+		{
+			j = 0;
+			while (argv[i + 1][j])
+			{
+				if (std::isalpha(argv[i + 1][j]))
+					std::cout << (char)std::toupper(argv[i + 1][j]);
+				else
+					std::cout << argv[i + 1][j];
+				j++;
+			}
+			std::cout << " ";
+			i++;
+		}
+		std::cout << "\n";
+	}
+}
